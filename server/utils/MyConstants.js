16821de86b03ef61347ff949fea0791ad9e5bfa1
supernhan <nhanboyll@gmail.com>
2023-10-20 22:25:34 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.ytafuty.mongodb.net',
+    DB_USER: 'lenhan',
+    DB_PASS: 'lengocnhan2003',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'lengocnhan',
+    JWT_EXPIRES: '60000', // in milliseconds
+    EMAIL_USER: 'nhanboyll@gmail.com', // gmail service
+    EMAIL_PASS: 'qxmswyatydibzznx'
+  };
+  module.exports = MyConstants;
